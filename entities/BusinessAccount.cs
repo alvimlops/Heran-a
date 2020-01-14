6187aaa3657f9952e0674faa80ebaebbb9fcc6c8
alvaro lopes <alvim.lopez@gmail.com>
2020-01-14 10:18:58 -0300

    primeiro commit

---
+++ entities/BusinessAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace herança.entities
+{
+    class BusinessAccount : account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+         
+        public void Loan(double amount)
+        {
+            if(amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+            
+        }
+
+
+
+
+    }
+}
---
+++ entities/account.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace herança.entities
+{
+    class account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+
+        public account()
+        {
+        }
+
+        public account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+
+
+
+
+
+    }
+}
